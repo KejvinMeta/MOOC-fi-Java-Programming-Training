52845d5257c86e0c7632f60e7c2baac768bb7f6c
Kejvin Meta <metakevi7@gmail.com>
2024-10-24 19:07:30 +0200

    Add files via upload

---
+++ part01-Part01_06.HiAdaLovelace/src/main/java/org/example/HiAdaLovelace.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class HiAdaLovelace {
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+
+        System.out.println("Hi " + name + "!");
+    }
+}
