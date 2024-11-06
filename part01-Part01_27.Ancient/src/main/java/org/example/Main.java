5b3737038549288c8ed2e14bfd26ff6812daa37b
Kejvin Meta <metakevi7@gmail.com>
2024-11-06 08:07:43 +0100

    Add files via upload

---
+++ part01-Part01_27.Ancient/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a year: ");
+        int year = Integer.valueOf(scanner.nextLine());
+
+        if(year < 2015){
+            System.out.println("Ancient history!");
+        }
+    }
+}
