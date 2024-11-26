accf9ab5c9467b870d4d1f4b5b35a32b4da56dd9
Kejvin Meta <metakevi7@gmail.com>
2024-11-26 18:39:17 +0100

    Add files via upload

---
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/com/sda/NumberOfNegativeNumbers.java
@@ -0,0 +1,27 @@
+package com.sda;
+import java.util.Scanner;
+
+public class NumberOfNegativeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfNegativeNumbers = 0;
+
+        while (true) {
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            if (number < 0) {
+                numberOfNegativeNumbers++;
+            }
+        }
+
+        System.out.println("Number of negative numbers: " + numberOfNegativeNumbers);
+    }
+}
+
+
