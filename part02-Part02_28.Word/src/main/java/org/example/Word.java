bd31d1f0f0923635358c87f90f24a31996ffce94
Kejvin Meta <metakevi7@gmail.com>
2024-12-17 14:19:11 +0100

    Add files via upload

---
+++ part02-Part02_28.Word/src/main/java/org/example/Word.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Word {
+    public static void main(String[] args) {
+        System.out.println("The word returned by the method word() is: " + word());
+    }
+
+    public static String word() {
+        return "java";
+    }
+}
