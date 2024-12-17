33c943ee6a2ee2d92e19f01cf1b16fafacc5f7e5
Kejvin Meta <metakevi7@gmail.com>
2024-12-17 14:18:35 +0100

    Add files via upload

---
+++ part02-Part02_27.NumberUno/src/main/java/org/example/NumberUno.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class NumberUno {
+    public static void main(String[] args) {
+        System.out.println("The value returned by numberUno() is: " + numberUno());
+    }
+
+    public static int numberUno(){
+        return 1;
+    }
+}
