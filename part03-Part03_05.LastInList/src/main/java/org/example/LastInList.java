46ebdd13ddaff5642a38a44575e0fdae17ee45d1
Kejvin Meta <metakevi7@gmail.com>
2024-12-26 08:06:02 +0100

    Add files via upload

---
+++ part03-Part03_05.LastInList/src/main/java/org/example/LastInList.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LastInList {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> names = new ArrayList<>();
+
+        while (true) {
+            String name = scanner.nextLine();
+
+            if (name.isEmpty()) {
+                break;
+            }
+
+            names.add(name);
+        }
+        if (!names.isEmpty()) {
+            System.out.println(names.get(names.size() - 1));
+        }
+    }
+}
