d873241ee0256ac1d910374abf5e7f7f1311974e
Kejvin Meta <metakevi7@gmail.com>
2025-02-17 16:35:25 +0100

    Add files via upload

---
+++ part03-Part03_22.PrintInStars/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class PrintInStars {
+    public static void main(String[] args) {
+        int[] array = {5,1,3,4,2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array) {
+        for 
+    }
+}
